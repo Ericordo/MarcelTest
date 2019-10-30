e992e54b25d9f8b138044df92d74f2289dd9d5c5
Eric Ordonneau <eric.ordonneau@outlook.fr>
2019-10-30 10:06:40 +0100

    Configured the Map and Location Manager. Added marker at user's current location.

--- MarcelTest/Controller/HomeViewController.swift
+++ MarcelTest/Controller/HomeViewController.swift
@@ -7,14 +7,90 @@
 //
 
 import UIKit
+import GoogleMaps
+
+
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    let defaultLocation = CLLocation(latitude: 48.85, longitude: 2.34)
+    let zoomLevel : Float = 15.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpLocationManager()
+        setUpMap()
+        
+        
     }
-
-
+    
+    private func setUpLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func setUpMap() {
+        mapView.delegate = self
+        let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude,
+                                              longitude: defaultLocation.coordinate.longitude,
+                                              zoom: zoomLevel)
+        mapView.camera = camera
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+    }
+    
+    
 }
 
+extension HomeViewController: GMSMapViewDelegate {
+    
+    
+}
+
+
+extension HomeViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        mapView.animate(to: camera)
+        let marker = GMSMarker(position: location.coordinate)
+        marker.icon = UIImage(named: "Pickup Marker")
+        marker.map = mapView
+        
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+    }
+    
+}
